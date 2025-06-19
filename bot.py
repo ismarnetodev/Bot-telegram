7c6b2817f2e191c98552d083e55b20c3634ec181
ismarnetodev <dsxzz41@gmail.com>
2025-06-18 23:04:13 -0300

    Otimizaçao na interaçao com o usuario na parte do /lang

--- bot.py
+++ bot.py
@@ -23,14 +23,14 @@ def comandos(msg):
 def lang(msg):
     texto = (
         "🌟 *Melhores linguagens de programação de 2025:*\n\n"
-        "*Python*: Versátil e popular, reina em áreas como IA, aprendizado de máquina, ciência de dados e automação. "
+        "*/Python*: Versátil e popular, reina em áreas como IA, aprendizado de máquina, ciência de dados e automação. "
         "Sua sintaxe clara e vasta gama de bibliotecas a tornam atraente para iniciantes e projetos complexos.\n\n"
-        "*JavaScript*: Indispensável para desenvolvimento web, com frameworks como React, Angular e Vue impulsionando "
+        "*/JavaScript*: Indispensável para desenvolvimento web, com frameworks como React, Angular e Vue impulsionando "
         "a criação de aplicações web dinâmicas e interativas.\n\n"
-        "*Go (Golang)*: Desenvolvida pelo Google, destaca-se pela sua performance e concorrência, sendo excelente para "
+        "*/Go (Golang)*: Desenvolvida pelo Google, destaca-se pela sua performance e concorrência, sendo excelente para "
         "sistemas distribuídos e aplicações backend de alta performance.\n\n"
-        "*Java*: Continua sendo uma linguagem robusta e amplamente utilizada em aplicações corporativas.\n\n"
-        "*PHP*: Popular no desenvolvimento web, especialmente em aplicações legadas."
+        "*/Java*: Continua sendo uma linguagem robusta e amplamente utilizada em aplicações corporativas.\n\n"
+        "*/PHP*: Popular no desenvolvimento web, especialmente em aplicações legadas."
     )
     bot.reply_to(msg, texto, parse_mode='Markdown')
 
@@ -43,23 +43,23 @@ def langyt(msg):
         )
     bot.reply_to(msg, texto, parse_mode='Markdown')
 
-@bot.message_handler(commands='python')
+@bot.message_handler(commands='Python')
 def Python(msg):
     bot.reply_to(msg, 'https://www.cursoemvideo.com/curso/python-3-mundo-1/')
 
-@bot.message_handler(commands='javaScript')
+@bot.message_handler(commands='JavaScript')
 def JavaScript(msg):
     bot.reply_to(msg, 'https://www.cursoemvideo.com/curso/javascript/')
 
-@bot.message_handler(commands='go')
+@bot.message_handler(commands='Go')
 def Go(msg):
     bot.reply_to(msg, 'https://www.youtube.com/watch?v=_MkQLDMak-4&list=PL5aY_NrL1rjucQqO21QH8KclsLDYu1BIg')
 
-@bot.message_handler(commands='java')
+@bot.message_handler(commands='Java')
 def Java(msg):
     bot.reply_to(msg, 'https://www.cursoemvideo.com/curso/java-basico/')
 
-@bot.message_handler(commands='php')
+@bot.message_handler(commands='PHP')
 def PHP(msg):
     bot.reply_to(msg, 'https://www.cursoemvideo.com/curso/curso-de-php-moderno-modulo-01/')
 
